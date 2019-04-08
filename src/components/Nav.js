8fffcabb967bb71f7a9a38bb25216276e5203d73
Eric Njogu <kunadawa@gmail.com>
2019-04-08 09:32:41 +0300

    feat: showing logout link, authed user name on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, {Component, Fragment} from 'react'
 import {NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
 
-export default function Nav() {
-    return (
-        <nav className='nav'>
-            <ul>
-                <li>
-                    <NavLink to='/' exact activeClassName='active'>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/signin' activeClassName='active'>
-                        Sign In
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+class Nav extends Component {
+    render() {
+        const {username} = this.props
+        return (
+            <nav className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to='/' exact activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    {username === null &&
+                        <li>
+                            <NavLink to='/signin' activeClassName='active'>
+                                Sign In
+                            </NavLink>
+                        </li>
+                    }
+                    {username !== null &&
+                        <Fragment>
+                            <li>
+                                Welcome, {username}
+                            </li>
+                            <li>
+                                <NavLink to='/signout'>
+                                    Sign Out
+                                </NavLink>
+                            </li>
+                        </Fragment>
+                    }
+
+                </ul>
+            </nav>
+        )
+    }
+
+
 }
+
+function mapStateToProps({authedUser, users}) {
+    const username = authedUser === null ? null : users[authedUser].name
+    return {
+        username
+    }
+}
+
+export default connect(mapStateToProps)(Nav)
