3212744a60653b9a2c47fc7b7dc0061043efe3e8
Eric Njogu <kunadawa@gmail.com>
2019-04-10 17:28:57 +0300

    feat: adding actions and action handlers for new questions & answers
    pattern borrowed from add/toggle tweet in https://github.com/kunadawa/reactnd-chirper-app

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,12 @@
-export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
+import {_saveQuestion, _saveQuestionAnswer} from '../utils/_DATA'
+import {showLoading, hideLoading} from 'react-redux-loading'
+import {addAnswerToUser, addQuestionToUser, removeAnswerFromUser} from '../actions/users'
+
+export const RECEIVE_QUESTIONS = "RECEIVE QUESTIONS"
+export const ANSWER_QUESTION = 'ANSWER QUESTION'
+export const SAVE_QUESTION = 'SAVE QUESTION'
+export const REMOVE_ANSWER = 'REMOVE ANSWER'
+
 
 /**
  * action creator
@@ -7,5 +15,50 @@ export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+export function saveQuestion(question) {
+    return {
+        type: SAVE_QUESTION,
+        question
+    }
+}
+
+export function handleSaveQuestion(question) {
+    return (dispatch) => {
+        dispatch(showLoading())
+
+        return _saveQuestion(question)
+            .then(savedQuestion => dispatch(saveQuestion(savedQuestion)))
+            .then(savedQuestion => dispatch(addQuestionToUser(savedQuestion)))
+            .then(() => dispatch(hideLoading()))
+    }
+}
+
+export function saveQuestionAnswer(answer) {
+    return {
+        type: ANSWER_QUESTION,
+        answer
+    }
+}
+
+export function removeQuestionAnswer(answer) {
+    return {
+        type: REMOVE_ANSWER,
+        answer
+    }
+}
+
+export function handleSaveQuestionAnswer (answer) {
+    return (dispatch) => {
+        dispatch(saveQuestionAnswer(answer))
+        dispatch(addAnswerToUser(answer))
+        return _saveQuestionAnswer(answer).catch((e) => {
+            console.warn('Error in handleSaveQuestionAnswer ', e)
+            dispatch(removeQuestionAnswer(answer))
+            dispatch(removeAnswerFromUser(answer))
+            alert(`There was an error answering the question. Please try again`)
+        })
     }
 }
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,11 +1,35 @@
 export const RECEIVE_USERS = "RECEIVE WATUS"
+export const ADD_QUESTION_TO_USER = "ADD_QUESTION_TO_USER"
+export const ADD_ANSWER_TO_USER = "ADD_ANSWER_TO_USER"
+export const REMOVE_ANSWER_FROM_USER = "REMOVE_ANSWER_FROM_USER"
 
-/**
+    /**
  * action creator
  */
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function addQuestionToUser(question) {
+    return {
+        type: ADD_QUESTION_TO_USER,
+        question
+    }
+}
+
+export function addAnswerToUser(answer) {
+    return {
+        type: ADD_ANSWER_TO_USER,
+        answer
+    }
+}
+
+export function removeAnswerFromUser(answer) {
+    return {
+        type: REMOVE_ANSWER_FROM_USER,
+        answer
     }
 }
